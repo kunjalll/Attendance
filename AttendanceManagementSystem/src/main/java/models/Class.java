0459b0d515b8ce2898b70aab6c8f7b0142631d15
kunjalll <101081061+kunjalll@users.noreply.github.com>
2023-03-30 13:12:37 +0545

    Add files via upload

---
+++ AttendanceManagementSystem/src/main/java/models/Class.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Class {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+
+    private int id;
+    private String classname;
+}
